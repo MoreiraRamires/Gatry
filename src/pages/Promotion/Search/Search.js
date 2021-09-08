df1d5b6d8b09d85f82612c96dfb54601ebcdec10
Rafael Ramires <moreira.ramires@gmail.com>
2021-09-08 15:43:43 -0300

    component UI Container

--- src/pages/Promotion/Search/Search.js
+++ src/pages/Promotion/Search/Search.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import PromotionSearch from 'components/Promotion/Search/Search';
+import UIContainer from 'components/UI/Container/Container';
 
 const PagesPromotionSearch = () => {
   
   return (
-    <div
-      style={{
-        maxWidth: 800,
-        margin: '30px auto',
-      }}
-    >
+    <UIContainer>
       <PromotionSearch/>
-    </div>
+    </UIContainer>
+   
   );
 };
 
