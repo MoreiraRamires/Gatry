bcd7940067da3c2e8eb7e45a285cb1c9a111cadc
Rafael Ramires <moreira.ramires@gmail.com>
2021-09-13 21:26:01 -0300

    implementando update na API

--- src/components/Promotion/Form/Form.js
+++ src/components/Promotion/Form/Form.js
@@ -1,4 +1,4 @@
-import React ,{useState}from 'react'
+import React ,{useEffect, useState}from 'react'
 import { useHistory } from 'react-router';
 import './Form.css';
 import axios from "axios";
@@ -9,10 +9,22 @@ const initialValue ={
   imageUrl:"",
   price:0,
 }
-const PromotionForm = () => {
+const PromotionForm = ({id}) => {
   const [values,setValues] = useState(initialValue);
   const history = useHistory()
-  console.log(values)
+
+
+useEffect(()=>{ // vou iniciar um request quando meu componente for montado. Igual na listagem
+  if(id){ // o id representa que estou trabalhando com uma edicao 
+    axios.get(`http://localhost:5000/promotions/${id}`)
+      .then((response) => {
+        
+        setValues(response.data);
+    })
+  }
+},[]);// o segundo parametro representa o que será ouvido para que o useEffect seja acionado novamente. O vazio significa que o hook sera acionado apenas quando o componente montar.
+
+
   function onChange(e){
     const {name,value} = e.target
     console.log({name,value})
@@ -22,7 +34,13 @@ const PromotionForm = () => {
 
   function onSubmit(e){
     e.preventDefault();
-    axios.post("http://localhost:5000/promotions",values)
+
+    const method = id ? 'put' : 'post';
+    const url = id
+    ?`http://localhost:5000/promotions/${id}`
+    :'http://localhost:5000/promotions'
+
+    axios[method](url,values)
     .then((response)=>{
       history.push("/") //redirecionar para a página de listagem - hook
     })
@@ -36,13 +54,14 @@ const PromotionForm = () => {
       <form onSubmit={onSubmit}>
         <div className="promotion-form__group">
           <label htmlFor="title">Titulo</label>
-          <input id="title"type="text" name="title" onChange={onChange}/>
+          <input id="title"type="text" name="title" onChange={onChange} value={values.title}/> 
+          {/* props value faz com que o campo seja preenchido com o ID */}
           <label htmlFor="price">Preço</label>
-          <input id="price"type="number" name="price" onChange={onChange}/>
+          <input id="price"type="number" name="price" onChange={onChange} value={values.price}/>
           <label htmlFor="url">Link</label>
           <input id="url" type="text" name="url"/>
           <label htmlFor="imageUrl">Imagem(url)</label>
-          <input id="url" type="text" name="imageUrl" onChange={onChange}/>
+          <input id="url" type="text" name="imageUrl" onChange={onChange} value={values.imageUrl}/>
         </div>
         <div className="promotion-form__save">
           <button type="submit">Salvar</button>
--- src/pages/Roots.js
+++ src/pages/Roots.js
@@ -11,9 +11,8 @@ const Root =()=>{
   return(
     <Router>
       <Switch>
-        <Route path="/create/:id/" component={PagesPromotionForm}/>
         <Route path="/create" component={PagesPromotionForm}/>
-
+        <Route path="/edit/:id/" component={PagesPromotionForm}/>
         <Route path="/" component={PagesPromotionSearch}/>
       </Switch>
     </Router>
